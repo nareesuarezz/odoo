c2fbacb0c44f185315b0bacd3e8117dda2bd35eb
= <=>
2024-02-15 19:11:11 +0000

    vuelta de 0

--- nareme/models/models.py
+++ nareme/models/models.py
@@ -22,21 +22,5 @@ class NaremeProyectos(models.Model):
             ultima_tarea = proyecto.tareas.sorted(key=lambda x: x.date_deadline, reverse=True)[:1]
             proyecto.ultima_tarea_fecha = ultima_tarea.date_deadline if ultima_tarea else None
 
-class NaremeConfigSettings(models.TransientModel):
-    _name = 'nareme.config.settings'
-    _inherit = 'res.config.settings'
 
-    show_tasks = fields.Boolean(string="Ver Tareas", config_parameter="project.tasks")
-
-    @api.model
-    def get_values(self):
-        res = super(NaremeConfigSettings, self).get_values()
-        ICPSudo = self.env['ir.config_parameter'].sudo()
-        show_tasks = ICPSudo.get_param('nareme.show_tasks', default=False)
-        res.update(show_tasks=show_tasks)
-        return res
-
-    def set_values(self):
-        super(NaremeConfigSettings, self).set_values()
-        ICPSudo = self.env['ir.config_parameter'].sudo()
-        ICPSudo.set_param("nareme.show_tasks", self.show_tasks)
+   
